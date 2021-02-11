74a28a7e725af467b1aa86326f641dcb6c5663cc
Meudje <ryankarl65@yahoo.fr>
2021-02-11 16:09:50 +0100

    ADD personService utilization

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Guide pour le TP2 :)
+
+# Build with dependencies for runnable application
+
+clean compile assembly:single
+
+# Compile and Run
+
+clean compile package exec:java
+
--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -8,12 +8,12 @@ import javax.naming.NamingException;
 import static utils.ContextUtils.createEjbProxy;
 import static utils.ContextUtils.createRemoteEjbContext;
 
-public class manager {
+public class Manager {
 
 
     public static void main(String[] args) {
 
-        Logger log = LogManager.getLogger(manager.class);
+        Logger log = LogManager.getLogger("Manager");
 
         log.info("Initialisation Remote Connexion");
 // Connection to Wildfly Server instance
@@ -29,23 +29,52 @@ public class manager {
         }
 
         final String appName = "ejb-remote";
-        final String moduleName = "PersonServiceImpl";
-        final String beanName = "ejb-remote";
+        final String moduleName = "";
+        final String beanName = "PersonServiceImpl";
         final String remoteInterface = "PersonInterface";
 
         String jndiEjbUrl = confJndiEjbUrl.getEjbUrl(appName, moduleName, beanName, remoteInterface);
 
         PersonInterface personService = null;
-
         try {
+
+
             personService = createEjbProxy(remotingContext, jndiEjbUrl, PersonInterface.class);
             log.info(" Connexion to Remote is Done !");
+
+            personService.sayHello();
+
+
+            personService.addName("Papa Moussa");
+            personService.addName("Richard");
+            personService.addName("...");
+
         } catch (NamingException e) {
             log.error("Error resolving bean");
         } catch (ClassCastException e) {
             log.error("Resolved EJB is of wrong type");
+        } catch (Exception ex) {
+            log.error("Je bug ici () {}", ex);
         }
 
+
+        String name = personService.getName(0);
+
+        personService.removeNameFromIndex(0);
+
+        try {
+            personService.getName(0);
+        } catch (Exception ex) {
+            log.error("Papa moussa n'existe plus !");
+        }
+
+        String nameTwo = personService.getName(1);
+        personService.removeAll();
+        String say = personService.sayHello();
+
+        System.out.println("voici le nom d'indice 0: \t " + name);
+        System.out.println("voici le nom d'indice 1: \t " + nameTwo);
+        System.out.println(say);
 
         log.info("Youpiiii !!");
     }
